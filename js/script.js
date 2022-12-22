d5622349b128d131183219987df4565b8db379b8
Marcin Izdebski <mizdebski123@gmail.com>
2022-12-23 00:08:57 +0100

    Added white and dark background

--- js/script.js
+++ js/script.js
@@ -1,15 +1,41 @@
-let button = document.querySelector(".button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
 
-button.addEventListener("click", () => {
-    body.classList.toggle("white");
+{
+    const toggleBackground = () => {
+        const conatainer = document.querySelector(".container");
+        const body = document.querySelector(".body");
+        const list = document.querySelectorAll(".navigation__item--link");
+        const button = document.querySelector(".container__button");
+        const buttonName = document.querySelector(".nextColorName");
 
-    if (body.classList.contains("white")) {
 
-        themeName.innerText = "White";
 
-    } else {
-        themeName.innerText = "Dark";
+        body.classList.toggle("white");
+        conatainer.classList.toggle("whiteBackground");
+        list.forEach(linkcolor => {
+            linkcolor.classList.toggle("whitelist");
+        })
+        button.classList.toggle("whiteButton");
+
+        if (conatainer.classList.contains("whiteBackground")) {
+            buttonName.innerText = "Dark";
+        }else {
+            buttonName.innerText = "White";
+
+        }
+
+
+
     }
-});
+
+    const init = () => {
+        const changeBackgroundButton = document.querySelector(".container__button");
+        changeBackgroundButton.addEventListener("click", toggleBackground);
+    }
+    init();
+}
+
+
+
+
+
+
